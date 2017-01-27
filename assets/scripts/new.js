5ed3cbbad30e17da8f8c0a407e7cfe9d80dce2cc
kinkorea <alexkarasik89@gmail.com>
2017-01-27 13:43:59 -0500

    Just committing for safety purposes

--- assets/scripts/new.js
+++ assets/scripts/new.js
@@ -18,6 +18,57 @@ const resetGameBoard = function() {
     $('.box').off('click');
   };
 
+  const checkWins = function() {
+    if (
+      board[0] === "X" && board[1] === "X" && board[2] === "X" ||
+      board[3] === "X" && board[4] === "X" && board[5] === "X" ||
+      board[6] === "X" && board[7] === "X" && board[8] === "X" ||
+      board[0] === "X" && board[4] === "X" && board[8] === "X" ||
+      board[2] === "X" && board[4] === "X" && board[6] === "X" ||
+      board[0] === "X" && board[3] === "X" && board[6] === "X" ||
+      board[1] === "X" && board[4] === "X" && board[7] === "X" ||
+      board[2] === "X" && board[5] === "X" && board[8] === "X"
+    ) {
+      //$('.win').text("X won!");
+      console.log('X won');
+      endGame();
+    } else if (
+      board[0] === "O" && board[1] === "O" && board[2] === "O" ||
+      board[3] === "O" && board[4] === "O" && board[5] === "O" ||
+      board[6] === "O" && board[7] === "O" && board[8] === "O" ||
+      board[0] === "O" && board[4] === "O" && board[8] === "O" ||
+      board[2] === "O" && board[4] === "O" && board[6] === "O" ||
+      board[0] === "O" && board[3] === "O" && board[6] === "O" ||
+      board[1] === "O" && board[4] === "O" && board[7] === "O" ||
+      board[2] === "O" && board[5] === "O" && board[8] === "O"
+    ) {
+      //$('.win').text("O won!");
+      console.log('O won');
+      endGame();
+    } else if (board.includes('') === false) {
+      $('.win').text("DRAW");
+      console.log('DRAW');
+      endGame();
+    }
+  };
+
+  let flipPlayer = function(index) {
+    if (board[index] === '') {
+      board[index] = currentPlayer;
+      checkWins();
+      if (currentPlayer === "X") {
+        currentPlayer = "O";
+        // checkWins();
+      } else {
+        currentPlayer = "X";
+        // checkWins();
+      }
+    } else {
+      console.log("pick another place");
+      //$('.win').text("pick another place");
+    }
+  };
+
   let boxes = $('.box');
   boxes.on('click', function(event) {
     if ($(event.target).text() === '') {
@@ -26,7 +77,7 @@ const resetGameBoard = function() {
       // changeTurn();
     }
     let conversion = parseInt(event.target.id);
-    turns(conversion);
+    flipPlayer(conversion);
     console.log(board);
   });
 
@@ -35,56 +86,9 @@ newGame.on('click', function() {
   resetGameBoard();
 });
 
-const checkWins = function() {
-  if (
-    board[0] === "X" && board[1] === "X" && board[2] === "X" ||
-    board[3] === "X" && board[4] === "X" && board[5] === "X" ||
-    board[6] === "X" && board[7] === "X" && board[8] === "X" ||
-    board[0] === "X" && board[4] === "X" && board[8] === "X" ||
-    board[2] === "X" && board[4] === "X" && board[6] === "X" ||
-    board[0] === "X" && board[3] === "X" && board[6] === "X" ||
-    board[1] === "X" && board[4] === "X" && board[7] === "X" ||
-    board[2] === "X" && board[5] === "X" && board[8] === "X"
-  ) {
-    //$('.win').text("X won!");
-    console.log('X won');
-    endGame();
-  } else if (
-    board[0] === "O" && board[1] === "O" && board[2] === "O" ||
-    board[3] === "O" && board[4] === "O" && board[5] === "O" ||
-    board[6] === "O" && board[7] === "O" && board[8] === "O" ||
-    board[0] === "O" && board[4] === "O" && board[8] === "O" ||
-    board[2] === "O" && board[4] === "O" && board[6] === "O" ||
-    board[0] === "O" && board[3] === "O" && board[6] === "O" ||
-    board[1] === "O" && board[4] === "O" && board[7] === "O" ||
-    board[2] === "O" && board[5] === "O" && board[8] === "O"
-  ) {
-    //$('.win').text("O won!");
-    console.log('O won');
-    endGame();
-  } else if (board.includes('') === false) {
-    $('.win').text("DRAW");
-    console.log('DRAW');
-    endGame();
-  }
-};
+
 // let count = 0;
-let turns = function(index) {
-  if (board[index] === '') {
-    board[index] = currentPlayer;
-    checkWins();
-    if (currentPlayer === "X") {
-      currentPlayer = "O";
-      // checkWins();
-    } else {
-      currentPlayer = "X";
-      // checkWins();
-    }
-  } else {
-    console.log("pick another place");
-    //$('.win').text("pick another place");
-  }
-};
+
 // let changeTurn = function() {
 //   if(currentPlayer === 'X'){
 //     currentPlayer = 'O';
@@ -113,7 +117,7 @@ module.exports = {
   checkWins,
   board,
   resetGameBoard,
-  turns,
+  flipPlayer,
   endGame,
 };
 // 'use strict';
