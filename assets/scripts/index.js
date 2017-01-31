4ccf338959f60b996798b73a8ccf50a580395443
kinkorea <alexkarasik89@gmail.com>
2017-01-31 15:06:17 -0500

    Fixed a grunt nag error in index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
-const game = require('./new');
+require('./new');
 
 $(() => {
   setAPIOrigin(location, config);
