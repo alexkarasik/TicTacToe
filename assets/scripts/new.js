764a2f0174378ebcd497e621548e2a85a01ae026
kinkorea <alexkarasik89@gmail.com>
2017-02-07 19:06:10 -0500

    Fixed a click issue with new boxes

--- assets/scripts/new.js
+++ assets/scripts/new.js
@@ -40,7 +40,7 @@ let possibleWins = function () {
       (board[2] === 'X' && board[4] === 'X' && board[6] === 'X')) {
     $('.message').text('X wins!');
     over =  true;
-
+    $('.board').off('click');
   } else if ((board[0] === 'O' && board[1] === 'O' && board[2] === 'O') ||
       (board[3] === 'O' && board[4] === 'O' && board[5] === 'O') ||
       (board[6] === 'O' && board[7] === 'O' && board[8] === 'O') ||
@@ -51,10 +51,12 @@ let possibleWins = function () {
       (board[2] === 'O' && board[4] === 'O' && board[6] === 'O')) {
     $('.message').text('O wins!');
     over = true;
+    $('.board').off('click');
   } else if (board.includes('') === false){
 
         $('.message').text('DRAW!!!');
         over = true;
+        $('.board').off('click');
   }
 };
 
